192205a0fda4d4be81c9943947a7732c037f5109
Wan Yau <lung@wans-mbp.home>
2018-10-30 11:46:54 +0000

    refactor main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,28 @@
 $(document).ready(function () {
-    $('#chooseFile').change(function () {
-        updateFiles(this.files);
-    });
+    new FileUploader();
+});
 
-    function updateFiles(files){
-        var flist = "";
-        var fileSizeExceeded = false;
-        for (var i = 0; i < files.length; i++) {
-            var warningSize = "";
-            if(files[i].size > 5242880){
-                warningSize = " <span style='color:red'>Warning! file size is over 5MB</span>";
-                fileSizeExceeded = true;
-            }
-            flist += '<li class="list-group-item">' + files[i].name + warningSize +'</li>'
-        }
-        $('#filesList').html(flist);
-        validateFiles(files, fileSizeExceeded);
-    }
+function FileUploader(){
+    var _this = this;
+	this.init();
+}
 
-    function validateFiles(files, fileSizeExceeded){
-        //show submit button if there are files
-        if (files.length > 0) {
-            $('#upload').css("display", "inline-block");
-        } else {
-            $('#upload').css("display", "none");
-        }
-        //show warning message if more than 10 files
-        if (files.length > 10) {
-            $('#warningText').text("Warning! There are more than 10 files!")
-        } else {
-            $('#warningText').text("");
-        }
-        //disable submit button if file size exceeded or more than 10 files
-        if(fileSizeExceeded || files.length > 10){
-            $("#upload").prop('disabled', true);
-        }else{
-            $("#upload").prop('disabled', false);
-        }
-    }
-
-    $('#upload').click(function () {
+FileUploader.prototype = {
+    init:function(){
+		var _this = this;
+	    this.setupListeners();
+    },
+    setupListeners:function(){
+        var _this = this;
+        $('#chooseFile').change(function () {
+            _this.updateFiles(this.files);
+        });
+        $('#upload').click(function () {
+            _this.submitFiles();
+        });
+    },
+    submitFiles:function(){
+        var _this = this;
         var filedata = document.getElementById("chooseFile");
         var formdata = false;
 
@@ -62,24 +45,56 @@ $(document).ready(function () {
                 data: formdata,
                 processData: false,
                 contentType: false,
-                success: successHandler,
+                success: function(res){
+                    if(res.files.length > 0){
+                        $('#chooseFile').val("");
+                        _this.updateFiles([]);
+                        _this.validateFiles([], false);
+                        for(var i = 0; i < res.files.length; i++){
+                            var file = res.files[i];
+                            $('#dbList').append('<a href="/files/'+file.id+'" class="list-group-item list-group-item-action">'+file.id+' - '+file.name+'</a>');
+                        }
+                    }
+                },
                 error: function (res) {
                     console.log("ERROR: " + res);
                 }
             });
         }
-    });
-
-    function successHandler(res){
-        console.log(res.files);
-        if(res.files.length > 0){
-            $('#chooseFile').val("");
-            updateFiles([]);
-            validateFiles([], false);
-            for(var i = 0; i < res.files.length; i++){
-                var file = res.files[i];
-                $('#dbList').append('<a href="/files/'+file.id+'" class="list-group-item list-group-item-action">'+file.id+' - '+file.name+'</a>');
+    },
+    updateFiles:function(files){
+        var _this = this;
+        var flist = "";
+        var fileSizeExceeded = false;
+        for (var i = 0; i < files.length; i++) {
+            var warningSize = "";
+            if(files[i].size > 5242880){
+                warningSize = " <span style='color:red'>Warning! file size is over 5MB</span>";
+                fileSizeExceeded = true;
             }
+            flist += '<li class="list-group-item">' + files[i].name + warningSize +'</li>'
+        }
+        $('#filesList').html(flist);
+        _this.validateFiles(files, fileSizeExceeded);
+    },
+    validateFiles: function(files, fileSizeExceeded){
+        //show submit button if there are files
+        if (files.length > 0) {
+            $('#upload').css("display", "inline-block");
+        } else {
+            $('#upload').css("display", "none");
+        }
+        //show warning message if more than 10 files
+        if (files.length > 10) {
+            $('#warningText').text("Warning! There are more than 10 files!")
+        } else {
+            $('#warningText').text("");
+        }
+        //disable submit button if file size exceeded or more than 10 files
+        if(fileSizeExceeded || files.length > 10){
+            $("#upload").prop('disabled', true);
+        }else{
+            $("#upload").prop('disabled', false);
         }
     }
-});
+}
